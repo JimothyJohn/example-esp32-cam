d65fb2c2763420d7a125543d0574e8997c8564f9
JimothyJohn <drunknmunky32@gmail.com>
2022-04-06 22:58:45 -0500

    First commit

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
-# ESP32 Cam and Edge Impulse
+# ESP-EYE and Edge Impulse
 
-How to run custom inference on a ESP32 cam using Edge Impulse. 
+How to run custom inference on a ESP-EYE using Edge Impulse. 
 
 ### Material
 
-![esp32-cam](Documentation/esp32-cam.jpg)
+![esp32-cam](docs/esp-eye.jpg)
 
-This code has been tested the AI Thinker ESP32 Cam module. It should work the same with the Wrover board or an board that has PSRAM. 
+This code has been tested on the ESP-EYE module. It should work the same with the Wrover board or an board that has PSRAM. 
 
 To use this board, please select your board in the Arduino code the following lines:
 
 ```
 // Select camera model
 
-#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
-//#define CAMERA_MODEL_ESP_EYE // Has PSRAM
+//#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
+#define CAMERA_MODEL_ESP_EYE // Has PSRAM
 //#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
 //#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
 //#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
@@ -27,52 +27,36 @@ To use this board, please select your board in the Arduino code the following li
 
 * Create your Image Classification model using [Edge Impulse](https://edgeimpulse.com).
 
-*Due to the board limitations, you may need to train your model with 96x96 images and use the MobileNetV1 0.01:*
+* Due to the board limitations you will need to train your model with 96x96 images and use the MobileNetV1 0.01:*
 
-![creat-impulse](Documentation/create-impulse.png)
+![create-impulse](docs/create-impulse.png)
 
 * Download the Arduino library under the `Deployment` tab in the Edge Impulse studio
-![dl-arduino-lib](Documentation/deployment-tab.png)
 
-### Basic Image Classification Example
+![dl-arduino-lib](docs/deployment-tab.png)
 
-*Note: On Sept 9th 2021, the issue predicting always the same classes has been fixed*
+### Image Classification Example
 
-* Open the `Basic-Image-Classification.ino` file under the `/Basic-Image-Classification` folder.
-* Import the .zip library you have downloaded from Edge Impulse Studio
-![import-zip](Documentation/import-zip.png)
-* Change the `#include <esp32-cam_image-classification_inference.h>` line according to your project name.
+* Save the .zip library you have downloaded to [lib/](lib/)
+* Add library location to [platformio.ini](platformio.ini)
+
+```ini
+lib_deps = lib/<your-project>.zip
+```
+
 * Compile and deploy the code to your board
+
+```bash
+pio run -t upload
+```
+
 * Open the serial monitor and use the provided IP to capture an image and run the inference:
-![serial-monitor](Documentation/open-serial.png)
-![inference](Documentation/inference.png)
 
-### Advanced Image Classification Example (Deprecated)
+![serial-monitor](docs/open-serial.png)
+![inference](docs/inference.png)
 
-*Note: 
-Deprecated since Basic Example has been fixed and also support bilinear interpolation technique to resize the frame.*
-
-*Note 2: 
-Here we use the ESP SDK to resize the image in RGB888 format using the bilinear interpolation technique. You can see the funtion declaration [on Espressif's Github repository](https://github.com/espressif/arduino-esp32/blob/master/tools/sdk/esp32/include/esp-face/image_util/include/image_util.h#L335)*.
-
-* Open the `Basic-Image-Classification.ino` file under the `/Advanced-Image-Classification` folder.
-* Set your WIFI credentials
-* Navigate to the `app_httpd.cpp` tab.
-* Import the .zip library you have downloaded from Edge Impulse Studio like on the previous example:
-![import-zip](Documentation/import-zip.png)
-* Change the `#include <euros_coins_classification_inference.h>` line according to your project name.
-* Compile and deploy the code to your board
-* Open the serial monitor and use the provided IP to capture an image and run the inference:
-![serial-monitor](Documentation/open-serial-2.png)
-* On your brower navigate to the IP provided by your Serial Console
-* Use the toggle button to activate the `Edge Impulse Inference` (you need to select an image resolution lower or equal to QVGA).
-* Click on `Run inference`:
-![inference-50c](Documentation/inference-50c.png)
-![inference-1e](Documentation/inference-1e.png)
-
-## Ressources
-
-*Note: Theses tutorials / repositories have been used to create this project:*
+## Resources
 
 - [TinyML ESP32-CAM: Edge Image classification with Edge Impulse](https://www.survivingwithandroid.com/tinyml-esp32-cam-edge-image-classification-with-edge-impulse/) 
 - [https://github.com/v12345vtm/esp32-cam-webserver-arduino-simplified-arduino-html](https://github.com/v12345vtm/esp32-cam-webserver-arduino-simplified-arduino-html)
+- [ESP32-CAM](https://github.com/edgeimpulse/example-esp32-cam)
--- src/Basic-Image-Classification.cpp
+++ src/Basic-Image-Classification.cpp
@@ -1,3 +1,4 @@
+// Source: https://github.com/edgeimpulse/example-esp32-cam/blob/main/Examples/Basic-Image-Classification/Basic-Image-Classification.ino
 /* Includes ---------------------------------------------------------------- */
 #include <Arduino.h>
 #include <WiFi.h>
@@ -14,104 +15,31 @@
 
 // Select camera model
 //#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
-//#define CAMERA_MODEL_ESP_EYE // Has PSRAM
+#define CAMERA_MODEL_ESP_EYE // Has PSRAM
 //#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
 //#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
 //#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
 //#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
-#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+//#define CAMERA_MODEL_AI_THINKER // Has PSRAM
 //#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
 
 #include "camera_pins.h"
 
 #define PART_BOUNDARY "123456789000000000000987654321"
 
-
-static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
-static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
-static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+static const char *_STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char *_STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char *_STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
 
 httpd_handle_t camera_httpd = NULL;
 httpd_handle_t stream_httpd = NULL;
 
-
-const char* ssid = "your-ssid";
-const char* password = "your-password";
+const char *ssid = "your-ssid";
+const char *password = "your-password";
 
 dl_matrix3du_t *resized_matrix = NULL;
 size_t out_len = EI_CLASSIFIER_INPUT_WIDTH * EI_CLASSIFIER_INPUT_HEIGHT;
 ei_impulse_result_t result = {0};
-
-void setup() {
-  Serial.begin(115200);
-  Serial.setDebugOutput(true);
-  Serial.println();
-
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
-  config.frame_size = FRAMESIZE_240X240;
-  config.jpeg_quality = 12;
-  config.fb_count = 1;
-
-#if defined(CAMERA_MODEL_ESP_EYE)
-  pinMode(13, INPUT_PULLUP);
-  pinMode(14, INPUT_PULLUP);
-#endif
-
-  // camera init
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
-    return;
-  }
-
-  sensor_t * s = esp_camera_sensor_get();
-  // initial sensors are flipped vertically and colors are a bit saturated
-  if (s->id.PID == OV3660_PID) {
-    s->set_vflip(s, 1); // flip it back
-    s->set_brightness(s, 1); // up the brightness just a bit
-    s->set_saturation(s, 0); // lower the saturation
-  }
-
-#if defined(CAMERA_MODEL_M5STACK_WIDE)
-  s->set_vflip(s, 1);
-  s->set_hmirror(s, 1);
-#endif
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-
-  startCameraServer();
-
-  Serial.print("Camera Ready! Use 'http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("' to connect");
-}
 
 int raw_feature_get_data(size_t offset, size_t out_len, float *signal_ptr)
 {
@@ -120,7 +48,8 @@ int raw_feature_get_data(size_t offset, size_t out_len, float *signal_ptr)
   size_t out_ptr_ix = 0;
 
   // read byte for byte
-  while (bytes_left != 0) {
+  while (bytes_left != 0)
+  {
     // grab the values and convert to r/g/b
     uint8_t r, g, b;
     r = resized_matrix->item[pixel_ix];
@@ -158,7 +87,8 @@ void classify()
   // print the predictions
   ei_printf("Predictions (DSP: %d ms., Classification: %d ms., Anomaly: %d ms.): \n",
             result.timing.dsp, result.timing.classification, result.timing.anomaly);
-  for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
+  for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++)
+  {
     ei_printf("    %s: \t%f\r\n", result.classification[ix].label, result.classification[ix].value);
   }
 #if EI_CLASSIFIER_HAS_ANOMALY == 1
@@ -166,14 +96,24 @@ void classify()
 #endif
 }
 
-static esp_err_t capture_handler(httpd_req_t *req) {
+static esp_err_t page_handler(httpd_req_t *req)
+{
+
+  httpd_resp_set_type(req, "text/html");
+  httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
+  // httpd_resp_send(req, page, sizeof(page));
+}
+
+static esp_err_t capture_handler(httpd_req_t *req)
+{
 
   esp_err_t res = ESP_OK;
-  camera_fb_t * fb = NULL;
+  camera_fb_t *fb = NULL;
 
   Serial.println("Capture image");
   fb = esp_camera_fb_get();
-  if (!fb) {
+  if (!fb)
+  {
     Serial.println("Camera capture failed");
     httpd_resp_send_500(req);
     return ESP_FAIL;
@@ -202,7 +142,7 @@ static esp_err_t capture_handler(httpd_req_t *req) {
   // --- Convert back the resized RGB888 frame to JPG to send it back to the web app ---
 
   Serial.println("Converting resized RGB888 frame to JPG...");
-  uint8_t * _jpg_buf = NULL;
+  uint8_t *_jpg_buf = NULL;
   fmt2jpg(resized_matrix->item, out_len, EI_CLASSIFIER_INPUT_WIDTH, EI_CLASSIFIER_INPUT_HEIGHT, PIXFORMAT_RGB888, 10, &_jpg_buf, &out_len);
 
   // --- Send response ---
@@ -213,9 +153,9 @@ static esp_err_t capture_handler(httpd_req_t *req) {
   httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
 
   res = httpd_resp_send(req, (const char *)_jpg_buf, out_len);
-  
+
   // --- Free memory ---
-  
+
   dl_matrix3du_free(resized_matrix);
   free(_jpg_buf);
   _jpg_buf = NULL;
@@ -223,33 +163,102 @@ static esp_err_t capture_handler(httpd_req_t *req) {
   return res;
 }
 
-static esp_err_t page_handler(httpd_req_t *req) {
-
-  httpd_resp_set_type(req, "text/html");
-  httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
-  // httpd_resp_send(req, page, sizeof(page));
-}
-
-void startCameraServer() {
+void startCameraServer()
+{
   httpd_config_t config = HTTPD_DEFAULT_CONFIG();
 
   httpd_uri_t capture_uri = {
-    .uri       = "/",
-    .method    = HTTP_GET,
-    .handler   = capture_handler,
-    .user_ctx  = NULL
-  };
-
+      .uri = "/",
+      .method = HTTP_GET,
+      .handler = capture_handler,
+      .user_ctx = NULL};
 
   Serial.printf("Starting web server on port: '%d'\n", config.server_port);
-  if (httpd_start(&camera_httpd, &config) == ESP_OK) {
+  if (httpd_start(&camera_httpd, &config) == ESP_OK)
+  {
     httpd_register_uri_handler(camera_httpd, &capture_uri);
-    //httpd_register_uri_handler(camera_httpd, &page_uri);
+    // httpd_register_uri_handler(camera_httpd, &page_uri);
   }
-
 }
 
-void loop() {
+void setup()
+{
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  // https://githubhot.com/index.php/repo/edgeimpulse/example-esp32-cam/issues/4
+  // config.frame_size = FRAMESIZE_96x96;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK)
+  {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t *s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID)
+  {
+    s->set_vflip(s, 1);      // flip it back
+    s->set_brightness(s, 1); // up the brightness just a bit
+    s->set_saturation(s, 0); // lower the saturation
+  }
+
+#if defined(CAMERA_MODEL_M5STACK_WIDE)
+  s->set_vflip(s, 1);
+  s->set_hmirror(s, 1);
+#endif
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  startCameraServer();
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+}
+
+void loop()
+{
   // put your main code here, to run repeatedly:
   delay(10);
 }
